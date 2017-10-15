f32e9f27385777b4d1f3d4e1a113eac0e2fd081e
toniYanakiev <yanakiev.t.2000@gmail.com>
2017-10-15 15:41:06 +0300

    changes

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -3,12 +3,13 @@ require 'csv'
 class FiltersController < ApplicationController
 	def filter
 		file = params[:file]
-		file_path = file.path;
-		filteredSum = 0;
+		file_path = file.path
+		filteredSum = 0
 		
 		CSV.foreach(file.path) do |row|
 			if row[2].to_i.odd?
 				filteredSum += row[1].to_f
+			end
 		end
 
 		filteredSum.ceil()
