98ac18921403cfb9fd61dd34db44f93b3fd74ace
toniYanakiev <yanakiev.t.2000@gmail.com>
2017-10-15 15:26:42 +0300

    filters

---
+++ app/controllers/filters_controller.rb
@@ -0,0 +1,17 @@
+require 'csv'
+
+class FiltersController < ApplicationController
+	def filter
+		file = params[:file]
+		file_path = file.path;
+		filteredSum = 0;
+		
+		CSV.foreach(file.path) do |row|
+			if row[2].to_i.odd?
+				filteredSum += row[1].to_f
+		end
+
+		filteredSum.ceil()
+		render plain: '%.2f' % filteredSum
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	get 'sums' => 'sums#sum'
 	post 'sums' => 'sums#create'
+	post 'filters' => 'filters#filter'
 	root 'sums#sum'
 end
